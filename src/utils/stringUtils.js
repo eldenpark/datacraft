7f4186193ce2553f6a52dd724574d7b6818fd1b2
enginehenryed <enginehenryed@gmail.com>
2017-08-16 02:11:26 +0900

    Add linearly converting processor with minify() in stringUtils

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -1,3 +1,6 @@
+/**
+ * ...
+ */
 const split = (text) => {
   var res = []
   var word = ''
@@ -18,6 +21,20 @@ const split = (text) => {
   return res
 };
 
+const REDUNDANT_WHITESPACE_REGEX = /(\s{2,})(?=(?:[^\'"]*[\'"][^\'"]*[\'"])*[^\'"]*$)/g;
+const CONTROLLING_SYMBOL_REGEX = /(\\r\\n|\\n|\\t|\r\n|\n)+(?=(?:[^\'"]*[\'"][^\'"]*[\'"])*[^\'"]*$)/g;
+const REDUNDANT_WHITESPACE_REGEX2 = /(\s{2,})/g;
+const CONTROLLING_SYMBOL_REGEX2 = /(\\r\\n|\\n|\\t|\r\n|\n)+/g;
+
+/**
+ * ...
+ */
+const minify = (text) => {
+  text = text.replace(CONTROLLING_SYMBOL_REGEX2, '');
+  return text.replace(REDUNDANT_WHITESPACE_REGEX2, ' ');
+}
+
 module.exports = {
-  split
+  split,
+  minify
 }
