ddf4f02d527c89d5210b0af6d74df90518dbb36d
engine <enginehenryed@gmail.com>
2017-09-10 02:32:17 +0900

    Define `task8` of `job1` to produce `Type3`
    Refs: #58 
    PR: #67

---
+++ jobs/job1/task8.js
@@ -0,0 +1,62 @@
+const { getRandomIntInclusive, getRandomInt } = require('../../src/utils/mathUtils');
+const { SEPARATOR } = require('../../src/constants');
+const { concat } = require('../../src/utils/stringUtils');
+
+const task8 = {};
+task8.name = 'task8'
+task8.config = {
+  stream: 'RW'
+}
+
+/**
+  Proper function argument (고유함수)
+
+  e.g.
+  acceleration: new Acceleration(e.x, e.y, e.z),
+  accelerationIncludingGravity: new Acceleration(e.x, e.y, e.z),
+ */
+task8.body = function (rl, store, done) {
+  const out = rl.output;
+  const numTokens = store.tokens.length;
+  const numLines = store.lines.length;
+
+  let text = undefined;
+  let randomIdx = undefined;
+  let randomToken1 = undefined;
+  let randomToken2 = undefined;
+  let randomToken3 = undefined;
+  let randomToken4 = undefined;
+  let randomLine = undefined;
+  let truth = undefined;
+  let queryLine = undefined;
+
+  for(let l = 0; l < 5; l++) {
+    text = '';
+    randomIdx = getRandomIntInclusive(0, 9);
+    randomToken1 = store.tokens[getRandomInt(0, numTokens)];
+    randomToken2 = store.tokens[getRandomInt(0, numTokens)];
+    randomToken3 = store.tokens[getRandomInt(0, numTokens)];
+    randomToken4 = store.tokens[getRandomInt(0, numTokens)];
+    randomToken5 = store.tokens[getRandomInt(0, numTokens)];
+
+    for (let i = 0; i < 10; i++) {
+      randomLine = store.lines[getRandomInt(0, numLines)];
+      if (i === randomIdx) {
+        truth = `${randomToken1}: new ${randomToken2}(${randomToken3}, ${randomToken4}, ${randomToken5});`;
+        text = (i === 0) ? truth : concat(text, SEPARATOR, truth);
+      } else {
+        text = (i === 0) ? randomLine : concat(text, SEPARATOR, randomLine);
+      }
+    }
+  
+    randomToken1 = store.tokens[getRandomInt(0, numTokens)];
+    queryLine = `${randomToken1}: new ${randomToken2}(${randomToken3}, ${randomToken4}, ${randomToken5});`;
+    
+    text = concat(text, SEPARATOR, queryLine);
+    text = concat(text, SEPARATOR, randomIdx);
+    out.write(`${text}\n`);
+  }
+  done(store);
+};
+
+exports.default = task8;
