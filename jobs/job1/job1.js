9676e51602f8aef0fc7e2e9b4ce3d977efffc67d
enginehenryed <enginehenryed@gmail.com>
2017-09-10 01:53:10 +0900

    Define `task5` of `job1` to produce `Type1_4`
    Refs: #55

--- jobs/job1/job1.js
+++ jobs/job1/job1.js
@@ -2,12 +2,13 @@ const task1 = require('./task1').default;
 const task2 = require('./task2').default;
 const task3 = require('./task3').default;
 const task4 = require('./task4').default;
+const task5 = require('./task5').default;
 
 const jobConfig = {
 }
 
 exports.default = {
   jobConfig,
-  tasks: [ task1, task2, task3, task4 ]
+  tasks: [ task1, task2, task3, task4, task5 ]
 };
 
