5f03577936df455f049d1aac8040bf6a3f25ddda
enginehenryed <enginehenryed@gmail.com>
2017-09-10 02:31:26 +0900

    Define `task8` of `job1` to produce `Type3`
    Refs: #58

--- jobs/job1/job1.js
+++ jobs/job1/job1.js
@@ -5,12 +5,13 @@ const task4 = require('./task4').default;
 const task5 = require('./task5').default;
 const task6 = require('./task6').default;
 const task7 = require('./task7').default;
+const task8 = require('./task8').default;
 
 const jobConfig = {
 }
 
 exports.default = {
   jobConfig,
-  tasks: [ task1, task2, task3, task4, task5, task6, task7 ]
+  tasks: [ task1, task2, task3, task4, task5, task6, task7, task8 ]
 };
 
